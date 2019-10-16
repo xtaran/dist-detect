ca1cd286ba7c1c40886d0e93b9d01c1e4b1c8b00
Axel Beckert <axel@ethz.ch>
2019-10-16 14:45:25 +0200

    Update newest-openssh-version-on-repology to handle recent entries with missing keys

--- bin/newest-openssh-version-on-repology.pl
+++ bin/newest-openssh-version-on-repology.pl
@@ -12,6 +12,7 @@ my $api_url = 'https://repology.org/api/v1/project/openssh';
 my $ua = Mojo::UserAgent->new;
 
 c( map { $_->{version} =~ s/_//r } # Some distributions use "X.Y_pZ"
-   grep { $_->{name} eq "openssh" and $_->{status} eq "newest" }
+   grep { exists($_->{name})      and exists($_->{status}) and
+          $_->{name} eq "openssh" and $_->{status} eq "newest" }
    @{ $ua->get($api_url)->result->json }
  )->uniq->join("\n")->say;
