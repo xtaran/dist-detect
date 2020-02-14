0913a8484b47084fd1dfc68adf88193d8a1ff9e3
Axel Beckert <axel@ethz.ch>
2020-02-14 11:11:21 +0100

    OpenSSH 8.2 is out

--- bin/interpret-versions.pl
+++ bin/interpret-versions.pl
@@ -29,7 +29,7 @@ use YAML::Any qw(LoadFile);
 use List::Util qw(uniq);
 
 # TODO: Needs to be determined automatically and stored in DB.
-my $latest = '8.1';
+my $latest = '8.2';
 
 my $schema_dir = "$Bin/../sql";
 my $db_dir = "$Bin/../db";
