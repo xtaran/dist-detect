22a040a36bb15d0b7eace1b0e730d2fac80213ec
Axel Beckert <axel@ethz.ch>
2024-05-07 13:18:55 +0200

    Fix repology JSON parsing
    Why did the API change if I still use the v1 API?

--- bin/newest-openssh-version-on-repology.pl
+++ bin/newest-openssh-version-on-repology.pl
@@ -12,7 +12,8 @@ my $api_url = 'https://repology.org/api/v1/project/openssh';
 my $ua = Mojo::UserAgent->new;
 
 c( map { $_->{version} =~ s/_//r } # Some distributions use "X.Y_pZ"
-   grep { exists($_->{name})      and exists($_->{status}) and
-          $_->{name} eq "openssh" and $_->{status} eq "newest" }
+   grep { ( ( exists($_->{name}) && $_->{name} eq "openssh" ) or
+            ( exists($_->{srcname}) and $_->{srcname} eq "openssh" ) ) and
+          exists($_->{status}) and $_->{status} eq "newest" }
    @{ $ua->get($api_url)->result->json }
  )->uniq->join("\n")->say;
