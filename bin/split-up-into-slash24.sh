dd0efc84db0b6334914a6c22648c5fbb3deece17
Axel Beckert <axel@ethz.ch>
2024-12-20 01:26:26 +0100

    Rewrite split-up-into-slash24.sh so that it also reads ranges from STDIN

--- bin/split-up-into-slash24.sh
+++ bin/split-up-into-slash24.sh
@@ -2,7 +2,7 @@
 
 # Split up CIDR ranges into /24
 
-for ip in "$@"; do
+splitup_cidr_ranges() {
     if echo "$ip" | egrep -q '/(2[4-9]|3[0-2])$' ; then
         echo -n "$ip "
     elif echo "$ip" | fgrep -q / ; then
@@ -10,4 +10,14 @@ for ip in "$@"; do
     else
         echo -n "$ip "
     fi
-done
+}
+
+if [ -n "$*" ] ; then
+    for ip in "$@"; do
+        splitup_cidr_ranges
+    done
+else
+    while read ip ; do
+        splitup_cidr_ranges
+    done
+fi
