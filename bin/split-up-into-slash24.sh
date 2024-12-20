b792d3a125045c3d2b34e077042324c5c01fa3e2
Axel Beckert <axel@ethz.ch>
2024-12-20 01:27:32 +0100

    split-up-into-slash24.sh: Drop replacing line-break with blank

--- bin/split-up-into-slash24.sh
+++ bin/split-up-into-slash24.sh
@@ -6,7 +6,7 @@ splitup_cidr_ranges() {
     if echo "$ip" | egrep -q '/(2[4-9]|3[0-2])$' ; then
         echo -n "$ip "
     elif echo "$ip" | fgrep -q / ; then
-        prips "$ip" | awk -F. '{print $1"."$2"."$3".0/24"}' | uniq | tr '\n' ' '
+        prips "$ip" | awk -F. '{print $1"."$2"."$3".0/24"}' | uniq
     else
         echo -n "$ip "
     fi
